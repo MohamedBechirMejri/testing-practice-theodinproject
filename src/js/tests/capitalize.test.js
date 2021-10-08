ce37a90e4b17cafa2f5fd2bb39f7c20a28a95d57
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-08 17:41:04 +0100

    add second test

--- src/js/tests/capitalize.test.js
+++ src/js/tests/capitalize.test.js
@@ -4,3 +4,7 @@ import capitalize from '../capitalize'
 it(`should return "Hello World!"`, () => {
   expect(capitalize('hello world!')).toBe('Hello world!');
 });
+
+it(`should return "something"`, () => {
+  expect(capitalize('theodinproject')).toBe('Theodinproject');
+});
