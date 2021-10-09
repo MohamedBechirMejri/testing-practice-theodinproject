53c66262098572b24636fd680c646a92b5033c5f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 11:59:34 +0100

    add test for z to a conversion

--- src/js/tests/caesar.test.js
+++ src/js/tests/caesar.test.js
@@ -1,8 +1,10 @@
 import caesar from '../caesar';
 
 describe('caesar', () => {
-    it(`should replace every letter with next letter in alphabet`, () => {
-      expect(caesar('abc')).toBe('bcd');
-    });
-
-})
+  it(`should replace every letter with next letter in alphabet`, () => {
+    expect(caesar('abc')).toBe('bcd');
+  });
+  it(`should replace z  with a`, () => {
+    expect(caesar('xyz')).toBe('yza');
+  });
+});
