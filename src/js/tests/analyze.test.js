f3f28e02ce16d2ebe42b7bdedd34d13eb791c7f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 18:46:42 +0100

    test for length property

--- src/js/tests/analyze.test.js
+++ src/js/tests/analyze.test.js
@@ -2,6 +2,9 @@ import analyze from '../analyze';
 
 describe('analyze', () => {
   it(`should return an object`, () => {
-    expect(typeof analyze()).toBe('object');
+    expect(typeof analyze([1, 8, 3, 4, 2, 6])).toBe('object');
+  });
+  it(`should return length of given array`, () => {
+    expect(analyze([1, 8, 3, 4, 2, 6]).length).toBe(6);
   });
 });
