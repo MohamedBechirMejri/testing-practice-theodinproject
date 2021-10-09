a9b6111368e1552ad16eae260ce748d06a035c71
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 18:59:29 +0100

    satisfy all tests

--- src/js/analyze.js
+++ src/js/analyze.js
@@ -1,5 +1,12 @@
-const analyze = (array) => ({
-        length: array.length
-    })
-
+const analyze = (array) => {
+  array.sort((a, b) => a - b); // ?
+  return {
+    average: array[Math.floor(array.length / 2)], // ?
+    min: array[0], // ?
+    max: array[array.length - 1], // ?
+    length: array.length, // ?
+  };
+};
 export default analyze;
+
+// ? is for Wallaby.js
