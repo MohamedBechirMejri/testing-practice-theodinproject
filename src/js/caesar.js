d8d19cdc8b5f862ba58320a7d861488aae807758
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 18:33:15 +0100

    use arrow function

--- src/js/caesar.js
+++ src/js/caesar.js
@@ -4,14 +4,15 @@ let a;
 
 const checkCase = (char) => (char === char.toLowerCase() ? alphabet : ALPHABET);
 
-function replaceLetter(array) {
+const replaceLetter = array => {
   const str = array;
   for (let i = 0; i < str.length; i += 1) {
     a = checkCase(str[i]);
     if (/[a-z]/i.test(str[i])) str[i] = a.charAt((a.indexOf(str[i]) + 1) % 26);
   }
   return str;
-}
+};
+
 const caesar = (string) => replaceLetter(string.split('')).join('');
 
 export default caesar;
