7355565e0811a62f4135482010bb3f74f5961e7c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 12:17:45 +0100

    satisfy case test

--- src/js/caesar.js
+++ src/js/caesar.js
@@ -1,9 +1,13 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
 const caesar = (string) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let a;
+
   const str = string.split('');
   for (let i = 0; i < str.length; i += 1) {
-    str[i] = alphabet.charAt(alphabet.indexOf(str[i]) + 1);
+    // eslint-disable-next-line no-unused-expressions
+    str[i] === str[i].toLowerCase() ? (a = alphabet) : (a = ALPHABET);
+    str[i] = a.charAt(a.indexOf(str[i]) + 1);
   }
 
   return str.join('');
