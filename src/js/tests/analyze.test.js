b25c1da7c1be52daeaf8b998061fd1cd0fba8b10
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 19:03:32 +0100

    add more tests

--- src/js/tests/analyze.test.js
+++ src/js/tests/analyze.test.js
@@ -16,4 +16,19 @@ describe('analyze', () => {
   it(`should return length of given array`, () => {
     expect(analyze([1, 8, 3, 4, 2, 6]).length).toBe(6);
   });
+
+  // more tests
+
+  it(`should return average of given array`, () => {
+    expect(analyze([100,500,1000,10000]).average).toBe(1000);
+  });
+  it(`should return max number in given array`, () => {
+    expect(analyze([100,500,1000,10000]).max).toBe(10000);
+  });
+  it(`should return min number in array`, () => {
+    expect(analyze([100,500,1000,10000]).min).toBe(100);
+  });
+  it(`should return length of given array`, () => {
+    expect(analyze([100,500,1000,10000]).length).toBe(4);
+  });
 });
