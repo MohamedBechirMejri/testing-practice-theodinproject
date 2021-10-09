5b2d03de8ce3d95510eddb8216519346d050af4a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 08:58:44 +0100

    add calculator.add() test

--- src/js/tests/calculator.test.js
+++ src/js/tests/calculator.test.js
@@ -3,3 +3,7 @@ import calculator from '../calculator';
 it(`should return "true"`, () => {
   expect(typeof calculator === 'object').toBe(true);
 });
+
+it(`should return "5"`, () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
