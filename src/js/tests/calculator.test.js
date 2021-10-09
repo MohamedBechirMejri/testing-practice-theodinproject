70d632560151838212feebf1d4f9216b34ab1b8a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-09 09:07:46 +0100

    add subtraction tests

--- src/js/tests/calculator.test.js
+++ src/js/tests/calculator.test.js
@@ -9,5 +9,13 @@ it(`should return "5"`, () => {
 });
 
 it(`should return "6"`, () => {
-  expect(calculator.add(3,3)).toBe(6);
+  expect(calculator.add(3, 3)).toBe(6);
+});
+
+it(`should return "1"`, () => {
+  expect(calculator.subtract(5, 4)).toBe(1);
+});
+
+it(`should return "0"`, () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
 });
